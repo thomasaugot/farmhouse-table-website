63aa127cfe66f595f25316796766570154a1b860
Thomas Augot <thomas.augot@hotmail.fr>
2023-03-10 15:27:05 +0100

    adding menu items to mainpage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -14,7 +14,7 @@ function Home() {
           }}
           alt="plate"
         />
-        <h1 style={{ color: "white", margin: "25px 5vw 0 5vw", lineHeight: "6vh" }}>
+        <h1 style={{ color: "white", margin: "35px 5vw 0 5vw", lineHeight: "6vh" }}>
           farmhouse table
         </h1>
         <h2 className="firstSubtitle">Exquisite dining since 1989</h2>
@@ -33,11 +33,12 @@ function Home() {
       >
         <img
           src={require("../../pages/Home/images/enjoyable-place-mobile@2x.jpg")}
+          className="phoneImg"
           style={{
             overflow: "visible",
             width: "80vw",
             height: "auto",
-            margin: "60px 0 0 0",
+            margin: "5vh 0 0 0",
             boxShadow: "0px 28px 55px -9px rgba(0,0,0,0.75)",
             position: "relative",
             bottom: "15vh",
@@ -53,10 +54,11 @@ function Home() {
         </p>
         <img
           src={require("../../pages/Home/images/locally-sourced-mobile@2x.jpg")}
+          className="phoneImg"
           style={{
             width: "80vw",
             height: "auto",
-            margin: "80px 0 ",
+            margin: "5vh 0 8vh 0",
             boxShadow: "0px 28px 55px -9px rgba(0,0,0,0.75)",
           }}
           alt="plate"
@@ -66,7 +68,92 @@ function Home() {
           All our ingredients come directly from our farm or local fishery. So you can be sure that
           you’re eating the freshest, most sustainable food.
         </p>
+        <div>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+        </div>
       </section>
+      <section style={{ backgroundColor: "black" }}>
+        <div>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+        </div>
+        <h2 className="whiteSubtitle">A few highlights from our menu</h2>
+        <p className="whiteBody">
+          We cater for all dietary requirements, but here’s a glimpse at some of our diner’s
+          favourites. Our menu is revamped every season.
+        </p>
+        <div>
+          <img
+            src={require("./images/salmon-mobile@2x.jpg")}
+            alt="plate"
+            className="phoneImg"
+            style={{
+              width: "80vw",
+              height: "auto",
+              margin: "5vh 0 8vh 0",
+              boxShadow: "0px 28px 55px -9px rgba(0,0,0,0.75)",
+            }}
+          />
+          <h3 className="whiteSubtitle menuItemTitle">Seared Salmon Fillet</h3>
+          <p className="whiteBody menuItemDescr">
+            Our locally sourced salmon served with a refreshing buckwheat summer salad.
+          </p>
+        </div>
+        <div>
+          <img
+            src={require("./images/beef-mobile@2x.jpg")}
+            alt="plate"
+            className="phoneImg"
+            style={{
+              width: "80vw",
+              height: "auto",
+              margin: "5vh 0 8vh 0",
+              boxShadow: "0px 28px 55px -9px rgba(0,0,0,0.75)",
+            }}
+          />
+          <h3 className="whiteSubtitle menuItemTitle">Rosemary Filet Mignon</h3>
+          <p className="whiteBody menuItemDescr">
+            Our prime beef served to your taste with a delicious choice of seasonal sides.
+          </p>
+        </div>
+        <div>
+          <img
+            src={require("./images/chocolate-mobile@2x.jpg")}
+            alt="plate"
+            className="phoneImg"
+            style={{
+              width: "80vw",
+              height: "auto",
+              margin: "5vh 0 8vh 0",
+              boxShadow: "0px 28px 55px -9px rgba(0,0,0,0.75)",
+            }}
+          />
+          <h3 className="whiteSubtitle menuItemTitle">Summer Fruit Chocolate Mousse</h3>
+          <p className="whiteBody menuItemDescr">
+            Creamy mousse combined with summer fruits and dark chocolate shavings.
+          </p>
+          <div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+          </div>
+        </div>
+      </section>
+      <section
+        style={{
+          backgroundColor: "white",
+        }}
+      ></section>
     </div>
   );
 }
