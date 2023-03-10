89a01fdeee15d9d880a7e8f8f9c57fdb8ada571d
Thomas Augot <thomas.augot@hotmail.fr>
2023-03-10 18:11:48 +0100

    setting up routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Home from "./pages/Home/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.scss";
+import Bookings from "./pages/Bookings/Bookings";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bookings" element={<Bookings />} />
+      </Routes>
     </div>
   );
 }
