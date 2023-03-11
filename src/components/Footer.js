bb67905f7541a1eeb8f23205e26707a6d09789f7
Thomas Augot <thomas.augot@hotmail.fr>
2023-03-11 10:46:18 +0100

    adding social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,9 @@ import "./Footer.scss";
 import "../styles/Home.scss";
 import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
 import { useMemo } from "react";
+import { FaFacebook } from "react-icons/fa";
+import { BsInstagram, BsWhatsapp } from "react-icons/bs";
+import { IconContext } from "react-icons";
 
 function Footer() {
   const { isLoaded } = useLoadScript({
@@ -26,6 +29,19 @@ function Footer() {
           </GoogleMap>
         )}
       </div>
+      <IconContext.Provider value={{ color: "white", size: "40px", className: "socialLogo" }}>
+        <div>
+          <a href="https://www.linkedin.com/in/thomas-augot/" target="_blank" rel="noreferrer">
+            <FaFacebook />
+          </a>
+          <a href="https://www.linkedin.com/in/thomas-augot/" target="_blank" rel="noreferrer">
+            <BsInstagram />
+          </a>
+          <a href="https://www.linkedin.com/in/thomas-augot/" target="_blank" rel="noreferrer">
+            <BsWhatsapp />
+          </a>
+        </div>
+      </IconContext.Provider>
     </div>
   );
 }
